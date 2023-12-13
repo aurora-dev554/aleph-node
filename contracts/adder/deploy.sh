aurora-dev554/aleph-node

commit 03af3c3e20ea1618e4fc3b6b81216060cd991ae4

A0-3627: Move liminal packages from _run-excluded-packages to liminal PR workflow (#1535)
# Description

Removed all liminal packages from `PR-commit` in
`_check-excluded-packages`, and moved them to workflow triggered by
`liminal-CI` label.

Also, fixed regression was introduced on `main` recently (20 minutes
global e2e timeout). And bumped `ink-dev` version in `adder` e2e test.

## Type of change

Please delete options that are not relevant.

- Bug fix (non-breaking change which fixes an issue)
- New feature (non-breaking change which adds functionality)

contracts/adder/deploy.sh
@@ -13,7 +13,7 @@ function run_ink_builder() {
     --name ink_builder \
     --platform linux/amd64 \
     --detach \
-    --rm public.ecr.aws/p6e8q1z1/ink-dev:1.0.0 sleep 1d 1>&2
+    --rm public.ecr.aws/p6e8q1z1/ink-dev:2.0.0 sleep 1d 1>&2
 }
 
 function ink_build() {

.github/scripts/run_checks_on_excluded_packages.sh
@@ -1,0 +1,77 @@
+#!/bin/bash
+
+set -eou pipefail
+
+function usage(){
+  cat << EOF
+Usage:
+   $0
+   This script is intended to run in CI in a job which aim is to perform static checks
+   on crates that are excluded from aleph-node workspace.
+   It runs static code analysis ie cargo clippy. On smart contracts crates, runs
+   cargo contract check.
+
+    --packages PACKAGES
+        List of aleph-node crates to check, in a below form
+        [aleph-client,baby-liminal-extension,benches/payout-stakers, ...]
+    [--skip-liminal]
+      optional: excluded liminal crates from checks
+EOF
+  exit 0
+}
+
+SKIP_LIMINAL="false"
+
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --packages)
+      PACKAGES="$2"
+      shift;shift
+      ;;
+    --skip-liminal)
+      SKIP_LIMINAL="true"
+      shift
+      ;;
+    *)
+      echo "Error: Unrecognized argument $1!"
+      exit 1
+      ;;
+  esac
+done
+
+if [[ -z "${PACKAGES}" ]]; then
+  echo "Error: --packages is required!"
+  exit 1
+fi
+
+packages_escaped=$(echo "${PACKAGES}" | sed -e 's/,/ /g' | tr -d '[]')
+packages=($packages_escaped)
+
+if [[ "${SKIP_LIMINAL}" == "true" ]]; then
+  # cargo clippy-liminal-extension is done in _liminal-checks-on-pr.yml
+  packages=("${packages[@]/baby-liminal-extension}")
+  packages=("${packages[@]/pallets\/baby-liminal}")
+  packages=("${packages[@]/poseidon}")
+  packages=("${packages[@]/relations\/ark\/src\/proc_macro}")
+  packages=("${packages[@]/relations\/ark}")
+  packages=("${packages[@]/relations\/jf}")
+fi
+echo ${packages[@]}
+
+for p in ${packages[@]}; do
+  echo "Checking package $p ..."
+  pushd "$p"
+
+  if [[ "$p" =~ .*contracts.* ]]; then
+     docker run \
+      --network host \
+      -v "$PWD:/code" \
+      -u "$(id -u):$(id -g)" \
+      --name ink_builder \
+      --platform linux/amd64 \
+      --rm public.ecr.aws/p6e8q1z1/ink-dev:2.0.0 cargo contract check
+  else
+    cargo clippy -- --no-deps -D warnings
+  fi
+  popd
+done

.github/scripts/run_e2e_test.sh
@@ -84,7 +84,7 @@ fi
 if [[ -n "${OUT_LATENCY:-}" ]]; then
     ARGS+=(-e OUT_LATENCY)
 fi
-timeout_duration="${TIMEOUT_MINUTES:-15m}"
+timeout_duration="${TIMEOUT_MINUTES:-20m}"
 echo "Running test, logs will be shown when tests finishes or after ${timeout_duration} timeout."
 # a hack to set global timeout on a e2e testcase run
 # we can't do that on GH yaml level due to https://github.com/actions/runner/issues/1979

.github/workflows/_build-test-node-and-runtime.yml
@@ -40,7 +40,7 @@ jobs:
 
       - name: Ensure liminal symbols are not present
         run: |
-          nm --demangle=rust target/production/aleph-node | grep -q liminal \
+          nm --demangle=rust target/release/aleph-node | grep -q liminal \
             && { echo "Liminal symbols are present"; exit 1; } \
             || echo "Liminal symbols are not present"
 

.github/workflows/_check-excluded-packages.yml
@@ -15,7 +15,7 @@ jobs:
       - name: Checkout source code
         uses: actions/checkout@v4
 
-      # TODO this is not ideal, as we need this step onlt to have rustup in the path
+      # TODO this is not ideal, as we need this step only to have rustup in the path
       # This step install Rust wfrom root rust-toolchain.toml, which is not used later
       # by this worfklow. Action install-rust-toolchain should have optional parameter path
       # in which we pass relative path to aleph-client to read its rust-toolchain.toml file
@@ -50,8 +50,9 @@ jobs:
           RUSTC_WRAPPER: ""
           RUSTC_WORKSPACE_WRAPPER: sccache
         run: |
-          ./scripts/run_checks_on_excluded_packages.sh \
-            "${{ steps.workspace-excluded-packages.outputs.value }}"
+          ./.github/scripts/run_checks_on_excluded_packages.sh \
+            --packages "${{ steps.workspace-excluded-packages.outputs.value }}" \
+            --skip-liminal
 
       - name: Assert that packages are compatible with no-std
         env:

.github/workflows/_liminal-checks-on-pr.yml
@@ -27,6 +27,14 @@ jobs:
         working-directory: baby-liminal-extension
         run: make clippy
 
+      - name: Check liminal excluded packages
+        env:
+          RUSTC_WRAPPER: ""
+          RUSTC_WORKSPACE_WRAPPER: sccache
+        run: |
+          ./.github/scripts/run_checks_on_excluded_packages.sh --packages \
+            [pallets/baby-liminal,poseidon,relations/ark,relations/ark/src/proc_macro,relations/jf]
+
   chain-extension-unit-tests:
     name: Run unit tests for the chain extension
     runs-on: [self-hosted, Linux, X64, large]

scripts/run_checks_on_excluded_packages.sh
@@ -1,45 +1,0 @@
-#!/bin/bash
-
-set -eo pipefail
-
-if [[ -n "${1}" ]]; then
-  # if the first arg passed (run via CI), it is of below form
-  # [aleph-client,baby-liminal-extension,benches/payout-stakers, ...]
-  list=$(echo "${1}" | sed -e 's/,/ /g' | tr -d '[]')
-  packages=($list)
-else
-  TOML_FILE="Cargo.toml"
-
-  # Read the TOML file and extract the `exclude` entries
-  packages=$(awk -F ' *= *' '/^exclude *= *\[/ {found=1} found && /^\]$/ {found=0} found' "$TOML_FILE")
-  packages="$(echo ${packages} | sed 's/[][,]/ /g' | sed 's/\s\+/\n/g' | sed '/^$/d')"
-
-  # Remove leading and trailing whitespace, and quotes from the entries
-  packages=$(echo "$packages" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e 's/^"//' -e 's/"$//')
-
-  packages="${packages//'%0A'/$'\n'}"
-
-  # Remove the key
-  packages=${packages:10}
-fi
-
-# cargo clippy-liminal-extension is done in _liminal-checks-on-pr.yml
-packages=("${packages[@]/baby-liminal-extension}")
-
-for p in ${packages[@]}; do
-  echo "Checking package $p ..."
-  pushd "$p"
-
-  if [[ "$p" =~ .*contracts.* ]]; then
-     docker run \
-      --network host \
-      -v "$PWD:/code" \
-      -u "$(id -u):$(id -g)" \
-      --name ink_builder \
-      --platform linux/amd64 \
-      --rm public.ecr.aws/p6e8q1z1/ink-dev:2.0.0 cargo contract check
-  else
-    cargo clippy -- --no-deps -D warnings
-  fi
-  popd
-done

scripts/run_local_pipeline.sh
@@ -5,5 +5,16 @@ set -e
 DIR=$(dirname "$0")
 
 "$DIR"/run_checks_on_aleph_node.sh
-"$DIR"/run_checks_on_excluded_packages.sh
+
+TOML_FILE="Cargo.toml"
+# Read the TOML file and extract the `exclude` entries
+packages=$(awk -F ' *= *' '/^exclude *= *\[/ {found=1} found && /^\]$/ {found=0} found' "$TOML_FILE")
+packages="$(echo ${packages} | sed 's/[][,]/ /g' | sed 's/\s\+/\n/g' | sed '/^$/d')"
+# Remove leading and trailing whitespace, and quotes from the entries
+packages=$(echo "$packages" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e 's/^"//' -e 's/"$//')
+packages="${packages//'%0A'/$'\n'}"
+# Remove the 'exclude' key
+packages=${packages:10}
+"$DIR"/.github/scripts/run_checks_on_excluded_packages.sh --packages ${packages[@]}
+
 "$DIR"/run_local_e2e_pipeline.sh

commit 7db177c0f9c45f482f49c7dd0677f54e2288ccab

A0-3627: Reverted ink-dev to 1.0.0 (#1539)
# Description

This PR reverts regression introduced in
https://github.com/Cardinal-Cryptography/aleph-node/commit/03af3c3e20ea1618e4fc3b6b81216060cd991ae4#diff-3a41020016879ea871e02f455267f8a057fbdde94bdfb5e3ba8e4cad2a2f2943L16

## Type of change

Please delete options that are not relevant.

- Bug fix (non-breaking change which fixes an issue)

# Testing:

*
https://github.com/Cardinal-Cryptography/aleph-node/actions/runs/7191635945

contracts/adder/deploy.sh
@@ -13,7 +13,7 @@ function run_ink_builder() {
     --name ink_builder \
     --platform linux/amd64 \
     --detach \
-    --rm public.ecr.aws/p6e8q1z1/ink-dev:2.0.0 sleep 1d 1>&2
+    --rm public.ecr.aws/p6e8q1z1/ink-dev:1.0.0 sleep 1d 1>&2
 }
 
 function ink_build() {
aurora-dev554/aleph-node

commit ef42bd57731aead19c3ed8ea9664e4abbf7523d8

A0-3150: bump to 1.0.0 (#1405)
# Description
Bump substrate to 1.0.0 branch.

* also bump of subxt was needed to 0.30
* bump of nightly toolchain used in aux crates

todo: test contracts migrations

## Type of change

Please delete options that are not relevant.

- Breaking change (fix or feature that would cause existing
functionality to not work as

baby-liminal-extension/Cargo.toml
@@ -15,7 +15,7 @@ scale = { package = "parity-scale-codec", version = "3", default-features = fals
 scale-info = { version = "2.3", default-features = false, features = ["derive"], optional = true }
 
 obce = { git = "https://github.com/727-Ventures/obce", rev = "5e3da417c2189ddd4e9ef82cd586f8ec94b8952a", default-features = false }
-sp-io = { git = "https://github.com/Cardinal-Cryptography/substrate.git", branch = "aleph-v0.9.43", default-features = false, features = ["disable_panic_handler", "disable_oom"] }
+sp-io = { git = "https://github.com/Cardinal-Cryptography/substrate.git", branch = "aleph-v1.0.0", default-features = false, features = ["disable_panic_handler", "disable_oom"] }
 
 pallet-baby-liminal = { path = "../pallets/baby-liminal", default-features = false, optional = true }
 

bin/node/src/main.rs
@@ -141,7 +141,7 @@ fn main() -> sc_cli::Result<()> {
             let runner = cli.create_runner(cmd)?;
             runner.sync_run(|config| {
                 if let frame_benchmarking_cli::BenchmarkCmd::Pallet(cmd) = cmd {
-                    cmd.run::<Block, ExecutorDispatch>(config)
+                    cmd.run::<Block, ()>(config)
                 } else {
                     Err(sc_cli::Error::Input("Wrong subcommand".to_string()))
                 }
@@ -150,7 +150,7 @@ fn main() -> sc_cli::Result<()> {
         #[cfg(not(feature = "runtime-benchmarks"))]
         Some(Subcommand::Benchmark) => Err(
             "Benchmarking wasn't enabled when building the node. You can enable it with \
-				     `--features runtime-benchmarks`."
+                    `--features runtime-benchmarks`."
                 .into(),
         ),
         None => {

aleph-client/Cargo.toml
@@ -20,11 +20,11 @@ thiserror = "1.0"
 serde_json = { version = "1.0.94" }
 contract-transcode = "2.1.0"
 ink_metadata = { version = "=4.0.1" }
-subxt = { version = "0.29.0", features = ["substrate-compat"] }
+subxt = { version = "0.30.1", features = ["substrate-compat"] }
 futures = "0.3.25"
 serde = { version = "1.0", features = ["derive"] }
 
-pallet-contracts-primitives = { git = "https://github.com/Cardinal-Cryptography/substrate.git", branch = "aleph-v0.9.43", default-features = false }
+pallet-contracts-primitives = { git = "https://github.com/Cardinal-Cryptography/substrate.git", branch = "aleph-v1.0.0", default-features = false }
 
 primitives = { path = "../primitives" }
 

benches/payout-stakers/Cargo.toml
@@ -14,10 +14,10 @@ log = "0.4"
 futures = "0.3.25"
 rand = "0.8.5"
 
-sp-core = { git = "https://github.com/Cardinal-Cryptography/substrate.git", branch = "aleph-v0.9.43", features = ["full_crypto"] }
-sp-keyring = { git = "https://github.com/Cardinal-Cryptography/substrate.git", branch = "aleph-v0.9.43" }
+sp-core = { git = "https://github.com/Cardinal-Cryptography/substrate.git", branch = "aleph-v1.0.0", features = ["full_crypto"] }
+sp-keyring = { git = "https://github.com/Cardinal-Cryptography/substrate.git", branch = "aleph-v1.0.0" }
 
-subxt = "0.29.0"
+subxt = "0.30.1"
 tokio = { version = "1.21.2", features = ["full"] }
 
 aleph_client = { path = "../../aleph-client" }

bin/cliain/Cargo.toml
@@ -21,8 +21,8 @@ serde_json = "1.0.81"
 
 tokio = { version = "1.21.2", features = ["full"] }
 
-pallet-staking = { git = "https://github.com/Cardinal-Cryptography/substrate.git", branch = "aleph-v0.9.43" }
-sp-core = { git = "https://github.com/Cardinal-Cryptography/substrate.git", branch = "aleph-v0.9.43", features = ["full_crypto"] }
+pallet-staking = { git = "https://github.com/Cardinal-Cryptography/substrate.git", branch = "aleph-v1.0.0" }
+sp-core = { git = "https://github.com/Cardinal-Cryptography/substrate.git", branch = "aleph-v1.0.0", features = ["full_crypto"] }
 
 aleph_client = { path = "../../aleph-client" }
 primitives = { path = "../../primitives" }

bin/cliain/src/main.rs
@@ -4,7 +4,7 @@ use aleph_client::{account_from_keypair, aleph_keypair_from_string, keypair_from
 use clap::Parser;
 use cliain::{
     bond, call, change_validators, finalize, force_new_era, instantiate, instantiate_with_code,
-    next_session_keys, nominate, owner_info, prepare_keys, prompt_password_hidden, remove_code,
+    next_session_keys, nominate, code_info, prepare_keys, prompt_password_hidden, remove_code,
     rotate_keys, schedule_upgrade, set_emergency_finalizer, set_keys, set_staking_limits, transfer,
     treasury_approve, treasury_propose, treasury_reject, update_runtime, upload_code, validate,
     vest, vest_other, vested_transfer, Command, ConnectionConfig,
@@ -44,7 +44,7 @@ fn read_seed(command: &Command, seed: Option<String>) -> String {
         | Command::NextSessionKeys { .. }
         | Command::RotateKeys
         | Command::SeedToSS58 { .. }
-        | Command::ContractOwnerInfo { .. } => String::new(),
+        | Command::ContractCodeInfo { .. } => String::new(),
         #[cfg(feature = "liminal")]
         Command::SnarkRelation { .. } => String::new(),
         _ => read_secret(seed, "Provide seed for the signer account:"),
@@ -212,10 +212,10 @@ async fn main() -> anyhow::Result<()> {
                 Err(why) => error!("Contract instantiate failed {:?}", why),
             }
         }
-        Command::ContractOwnerInfo(command) => {
+        Command::ContractCodeInfo(command) => {
             println!(
                 "{:#?}",
-                owner_info(cfg.get_connection().await, command).await
+                code_info(cfg.get_connection().await, command).await
             )
         }
         Command::ContractRemoveCode(command) => {

bin/finalizer/Cargo.toml
@@ -9,7 +9,7 @@ anyhow = "1.0"
 clap = { version = "3.0", features = ["derive"] }
 dialoguer = "0.10.0"
 hex = "0.4.3"
-subxt = "0.29.0"
+subxt = "0.30.1"
 tokio = { version = "1.21.2", features = ["full"] }
 aleph_client = { path = "../../aleph-client" }
 futures = "0.3.28"

commit 4f3e806059eb07b1253e725b4a4d51e715181150

A0-3858: Benchmark on-chain verifier (1/2) (#1583)
# Description

We run benchmarks for the on-chain verifier. 

Even though the actual verification happens in the
`aleph-runtime-interfaces` crate, we have to put them in the chain
extension library for two reasons:
1. we already have benchmarking setup ready for the CE and it is CE,
that is the only consumer of these metrics
1. additionally, we need to measure verification key access

For now, we use only a single circuit. The follow-up PR will extend
measurements for other layouts.

There was also a very nasty bug introduced in #1405 which turned off
custom host functions from the benchmarking node. It has been fixed in
this PR.

For the people who can read halo2 diagrams, a diagram of a circuit
<5,3>:

![image](https://github.com/Cardinal-Cryptography/aleph-node/assets/27450471/e584851e-2311-4667-84c0-64f893beb564)


## Type of change

- Bug fix (non-breaking change which fixes an issue)
- New feature (non-breaking change which adds functionality)

# Checklist:

- I have made neccessary updates to the Infrastructure
- I have made corresponding changes to the existing documentation
- I have created new documentation

baby-liminal-extension/Cargo.toml
@@ -35,6 +35,9 @@ frame-benchmarking = { default-features = false, git = "https://github.com/Cardi
 [dev-dependencies]
 aleph-runtime = { path = "../bin/runtime", features = ["liminal"] }
 
+[build-dependencies]
+halo2_proofs = { git = "https://github.com/Cardinal-Cryptography/pse-halo2", branch = "aleph", default-features = false, features = ["mock-kzg-params"] }
+
 [features]
 # This is an artificial feature that is used by 3rd party crates in macro expansions.
 std = []

baby-liminal-extension/benchmark-resources/.gitignore
@@ -1,0 +1,4 @@
+# Ignore everything in this directory
+*
+# Except this file
+!.gitignore

baby-liminal-extension/build.rs
@@ -1,0 +1,252 @@
+//! This build script is used to generate the SNARK artifacts for the benchmarking purposes.
+//!
+//! # Why build script?
+//!
+//! Benchmarks are run within the runtime environment. This means that:
+//! - cryptography is less effective there,
+//! - we don't have access to no-std funcionalities, which means that it is hard to run halo2 prover there (for proof
+//! generation)
+//!
+//! To overcome these problems, we generate the artifacts during building the crate and then use them in the runtime.
+//!
+//! # What is generated?
+//!
+//! For every circuit, we generate the following artifacts:
+//! - verification key,
+//! - proof,
+//! - public input.
+//!
+//! All of them are saved to corresponding files in the `benchmark-resources` directory as raw bytes.
+//!
+//! # How to run?
+//!
+//! You just have to build this crate with the `runtime-benchmarks` feature enabled. This will generate the artifacts
+//! and put them in the `benchmark-resources` directory. Changing the build script will trigger the artifacts generation
+//! again. On the other hand, changing any other file in this crate will not be considered as a reason for rerunning.
+//!
+//! # What circuits are generated?
+//!
+//! We provide a generic circuit that can be parametrized with the number of instances and the number of rows. More
+//! specifically, `BenchCircuit<INSTANCES, ROW_BLOWUP>` is a circuit that:
+//! - has `INSTANCES` instances (public inputs);
+//! - has `INSTANCES` advices (private inputs); `i`th advice is a square root of `i`th instance;
+//! - has `INSTANCES · ROW_BLOWUP` gates (rows);
+//!
+//! ## Gates
+//!
+//! First `INSTANCES` gates are the ones that ensure that the corresponding advice is indeed a square root of the
+//! corresponding instance.
+//!
+//! The rest of the gates are batches of `ROW_BLOWUP - 1` copies of the `i`th gate (`i`th batch corresponds to the `ith`
+//! gate).
+
+/// This build script is used only for the runtime benchmarking setup. We don't need to do anything here in other case.
+#[cfg(not(feature = "runtime-benchmarks"))]
+fn main() {}
+
+#[cfg(feature = "runtime-benchmarks")]
+use {
+    artifacts::generate_artifacts,
+    halo2_proofs::{halo2curves::bn256::Bn256, poly::kzg::commitment::ParamsKZG},
+    std::{env, fs, path::Path},
+};
+
+#[cfg(feature = "runtime-benchmarks")]
+fn main() {
+    // We rerun the build script only if this file changes. SNARK artifacts generation doesn't depend on any of the
+    // source files.
+    println!("cargo:rerun-if-changed=build.rs");
+
+    // We run benchmarks for up to ~4K gates - this is to be changed for the final version. Now, we keep it low for
+    // developer convenience.
+    const CIRCUIT_MAX_K: u32 = 12;
+    // We run a common setup for all generated circuits.
+    let params = ParamsKZG::<Bn256>::setup(CIRCUIT_MAX_K, ParamsKZG::<Bn256>::mock_rng());
+
+    let artifacts = generate_artifacts::<5, 3>(&params);
+
+    let path = |suf| {
+        Path::new(&env::current_dir().unwrap())
+            .join("benchmark-resources")
+            .join(format!("5_3_{suf}"))
+    };
+
+    fs::write(&path("vk"), artifacts.verification_key).unwrap();
+    fs::write(&path("proof"), artifacts.proof).unwrap();
+    fs::write(&path("input"), artifacts.public_input).unwrap();
+}
+
+/// This module contains the code that is used to generate the SNARK artifacts (proof generation).
+#[cfg(feature = "runtime-benchmarks")]
+mod artifacts {
+    use halo2_proofs::{
+        halo2curves::bn256::{Bn256, Fr, G1Affine},
+        plonk::{create_proof, keygen_pk, keygen_vk, VerifyingKey},
+        poly::{
+            commitment::Params,
+            kzg::{commitment::ParamsKZG, multiopen::ProverGWC},
+        },
+        transcript::{Blake2bWrite, Challenge255, TranscriptWriterBuffer},
+    };
+
+    use crate::circuit::BenchCircuit;
+
+    pub struct Artifacts {
+        pub verification_key: Vec<u8>,
+        pub proof: Vec<u8>,
+        pub public_input: Vec<u8>,
+    }
+
+    /// Run the proof generation for the given circuit and parameters.
+    pub fn generate_artifacts<const INSTANCES: usize, const ROW_BLOWUP: usize>(
+        params: &ParamsKZG<Bn256>,
+    ) -> Artifacts {
+        let circuit = BenchCircuit::<INSTANCES, ROW_BLOWUP>::natural_numbers();
+        let instances = (0..INSTANCES)
+            .map(|i| Fr::from((i * i) as u64))
+            .collect::<Vec<_>>();
+
+        let vk = keygen_vk(params, &circuit).expect("vk should not fail");
+        let pk = keygen_pk(params, vk.clone(), &circuit).expect("pk should not fail");
+
+        let mut transcript = Blake2bWrite::<_, _, Challenge255<_>>::init(vec![]);
+        create_proof::<_, ProverGWC<'_, Bn256>, _, _, _, _>(
+            params,
+            &pk,
+            &[circuit],
+            &[&[&instances]],
+            ParamsKZG::<Bn256>::mock_rng(),
+            &mut transcript,
+        )
+        .expect("prover should not fail");
+
+        Artifacts {
+            verification_key: serialize_vk(vk, params.k()),
+            proof: transcript.finalize(),
+            public_input: instances.iter().flat_map(|i| i.to_bytes()).collect(),
+        }
+    }
+
+    /// Serializes the verification key to the raw bytes together with the upperbound on the number of gates (required
+    /// by the on-chain verifier).
+    fn serialize_vk(vk: VerifyingKey<G1Affine>, k: u32) -> Vec<u8> {
+        let mut buffer = Vec::new();
+        buffer.extend(k.to_le_bytes());
+        buffer.extend(vk.to_bytes(halo2_proofs::SerdeFormat::RawBytesUnchecked));
+        buffer
+    }
+}
+
+/// This module defines the circuit for the benchmarking purposes.
+#[cfg(feature = "runtime-benchmarks")]
+mod circuit {
+    use halo2_proofs::{
+        circuit::{Layouter, Region, Value},
+        halo2curves::bn256::Fr,
+        plonk::{Circuit, ConstraintSystem, Error},
+        standard_plonk::{StandardPlonk, StandardPlonkConfig},
+    };
+
+    pub struct BenchCircuit<const INSTANCES: usize, const ROW_BLOWUP: usize> {
+        /// The roots of the instances (`i`th root is a square root of `i`th instance).
+        roots: [Fr; INSTANCES],
+    }
+
+    impl<const INSTANCES: usize, const ROW_BLOWUP: usize> Default
+        for BenchCircuit<INSTANCES, ROW_BLOWUP>
+    {
+        fn default() -> Self {
+            BenchCircuit {
+                roots: [Fr::zero(); INSTANCES],
+            }
+        }
+    }
+
+    impl<const INSTANCES: usize, const ROW_BLOWUP: usize> BenchCircuit<INSTANCES, ROW_BLOWUP> {
+        /// Create a circuit with the consecutive natural numbers as advices.
+        pub fn natural_numbers() -> Self {
+            let roots: Vec<_> = (0..INSTANCES).map(|i| Fr::from(i as u64)).collect();
+            Self {
+                roots: roots.try_into().unwrap(),
+            }
+        }
+
+        /// Assign the `idx`th root to the `a` and `b` advices and `-1` to the `q_ab` fixed value.
+        ///
+        /// We assume here that this is the first row in the region (offset 0).
+        fn neg_root_square(
+            &self,
+            idx: usize,
+            region: &mut Region<Fr>,
+            config: &StandardPlonkConfig<Fr>,
+        ) -> Result<(), Error> {
+            region.assign_advice(|| "root", config.a, 0, || Value::known(self.roots[idx]))?;
+            region.assign_advice(|| "root", config.b, 0, || Value::known(self.roots[idx]))?;
+            region.assign_fixed(
+                || "root selector",
+                config.q_ab,
+                0,
+                || Value::known(-Fr::one()),
+            )?;
+            Ok(())
+        }
+    }
+
+    impl<const INSTANCES: usize, const ROW_BLOWUP: usize> Circuit<Fr>
+        for BenchCircuit<INSTANCES, ROW_BLOWUP>
+    {
+        type Config = <StandardPlonk as Circuit<Fr>>::Config;
+        type FloorPlanner = <StandardPlonk as Circuit<Fr>>::FloorPlanner;
+
+        fn without_witnesses(&self) -> Self {
+            BenchCircuit::default()
+        }
+
+        fn configure(meta: &mut ConstraintSystem<Fr>) -> Self::Config {
+            StandardPlonk::configure(meta)
+        }
+
+        fn synthesize(
+            &self,
+            config: Self::Config,
+            mut layouter: impl Layouter<Fr>,
+        ) -> Result<(), Error> {
+            for instance_idx in 0..INSTANCES {
+                // For every instance, we ensure that the corresponding advice is indeed a square root of it.
+                layouter.assign_region(
+                    || format!("check {instance_idx}-th root"),
+                    |mut region| self.neg_root_square(instance_idx, &mut region, &config),
+                )?;
+            }
+
+            for instance_idx in 0..INSTANCES {
+                // We also do some dummy work to blow up the number of rows.
+                for copy in 0..(ROW_BLOWUP - 1) {
+                    layouter.assign_region(
+                        || format!("check {instance_idx}-th root ({}/{})", copy + 1, ROW_BLOWUP),
+                        |mut region| {
+                            self.neg_root_square(instance_idx, &mut region, &config)?;
+
+                            region.assign_advice_from_instance(
+                                || "copied instance",
+                                config.instance,
+                                instance_idx,
+                                config.c,
+                                0,
+                            )?;
+                            region.assign_fixed(
+                                || "copied instance selector",
+                                config.q_c,
+                                0,
+                                || Value::known(Fr::one()),
+                            )?;
+                            Ok(())
+                        },
+                    )?;
+                }
+            }
+
+            Ok(())
+        }
+    }
+}

baby-liminal-extension/src/backend/benchmarking.rs
@@ -12,14 +12,30 @@
 //! Since we don't have a pallet, we have to provide these two things ourselves. We do this by defining two dummy items.
 //! To avoid confusion outside this module, we reexport the `Pallet` as `ChainExtensionBenchmarking` type, which can be
 //! then used in the runtime benchmarking setup.
+//!
+//! # Expectations from the runtime
+//!
+//! Benchmarks are run for a specific runtime instance. We can refer to it via the `T` type in the benchmark body. Since
+//! sometimes we might require that `T` includes some pallet (e.g. `pallet_vk_storage`). We can put this constraint on
+//! our artificial `Config` trait.
+//!
+//! ## Note
+//!
+//! Please note, that in the current form, it would be sufficient to just use the `VkStorage` pallet as the `Pallet`
+//! type and `pallete_vk_storage::Config` as the `Config` trait. However, we want to keep the benchmarking of the
+//! chain extension abstracted from the pallets that it uses. This is why we define our own dummy pallet and config.
 
 use frame_benchmarking::v2::*;
+use frame_support::{sp_runtime::traits::Hash, BoundedVec};
+use pallet_vk_storage::{KeyHasher, VerificationKeys};
 use sp_std::vec;
 
+use crate::args::VerifyArgs;
+
 /// Dummy trait that defines the pallet's configuration. Since `auto trait` is not stable yet, we just provide a full
-/// blanket implementation for all types.
-trait Config {}
-impl<T> Config for T {}
+/// blanket implementation for all runtimes that contain the `pallet_vk_storage` pallet.
+trait Config: pallet_vk_storage::Config {}
+impl<T: pallet_vk_storage::Config> Config for T {}
 
 /// Dummy pallet struct. The only thing that actually matters is that it is generic over some type `T` that implements
 /// the `Config` trait.
@@ -31,12 +47,38 @@ pub struct Pallet<T> {
 /// limits the confusion to this module only.
 pub type ChainExtensionBenchmarking<T> = Pallet<T>;
 
+/// Get the verification artifacts from the benchmark resources.
+///
+/// Since the benchmarks are run within the runtime, we don't have access to the common `std::fs` utilities.
+/// Fortunately, we can still make use of the `include_bytes` macro.
+fn get_verification_artifacts<T: pallet_vk_storage::Config>() -> (
+    VerifyArgs,
+    BoundedVec<u8, <T as pallet_vk_storage::Config>::MaximumKeyLength>,
+) {
+    // We use a macro here, because a function cannot put literal variables in the `include_bytes` macro.
+    macro_rules! get {
+        ($art:literal) => {
+            include_bytes!(concat!("../../benchmark-resources/5_3_", $art)).to_vec()
+        };
+    }
+
+    let verification_key = get!("vk");
+    (
+        VerifyArgs {
+            verification_key_hash: KeyHasher::hash(&verification_key),
+            proof: get!("proof"),
+            public_input: get!("input"),
+        },
+        verification_key.try_into().unwrap(),
+    )
+}
+
 #[benchmarks]
 mod benchmarks {
     use scale::{Decode, Encode};
 
     use super::*;
-    use crate::args::VerifyArgs;
+    use crate::{args::VerifyArgs, backend::BackendExecutorT};
 
     /// Benchmark `verify` arguments decoding.
     #[benchmark]
@@ -57,12 +99,18 @@ mod benchmarks {
         }
     }
 
-    /// Benchmark proof verification.
+    /// Benchmark proof verification (covering both reading the verification key from the storage and the actual
+    /// verification).
     ///
     /// Due to macro internals, we cannot name the benchmark just `verify`.
     #[benchmark]
     fn verify_proof() {
+        let (args, verification_key) = get_verification_artifacts::<T>();
+        VerificationKeys::<T>::insert(args.verification_key_hash, verification_key);
+
         #[block]
-        {}
+        {
+            <T as BackendExecutorT>::verify(args).unwrap();
+        }
     }
 }

baby-liminal-extension/src/backend/executor.rs
@@ -14,7 +14,7 @@ pub trait MinimalRuntime: VkStorageConfig + ContractsConfig {}
 impl<R: VkStorageConfig + ContractsConfig> MinimalRuntime for R {}
 
 /// Default implementation for the chain extension mechanics.
-impl<Runtime: MinimalRuntime> BackendExecutor for Runtime {
+impl<Runtime: VkStorageConfig> BackendExecutor for Runtime {
     fn verify(args: VerifyArgs) -> Result<(), VerifierError> {
         let verifying_key = VerificationKeys::<Runtime>::get(args.verification_key_hash)
             .ok_or(VerifierError::UnknownVerificationKeyIdentifier)?

baby-liminal-extension/src/backend/weights.rs
@@ -1,7 +1,7 @@
 //! Autogenerated weights for baby_liminal_extension
 //!
 //! THIS FILE WAS AUTO-GENERATED USING THE SUBSTRATE BENCHMARK CLI VERSION 4.0.0-dev
-//! DATE: 2023-12-14, STEPS: `20`, REPEAT: 5, LOW RANGE: `[]`, HIGH RANGE: `[]`
+//! DATE: 2024-01-25, STEPS: `20`, REPEAT: 5, LOW RANGE: `[]`, HIGH RANGE: `[]`
 //! EXECUTION: , WASM-EXECUTION: Compiled, CHAIN: Some("./liminal-benchmark-chainspec.json"), DB CACHE: 1024
 
 // Executed Command:
@@ -64,14 +64,17 @@ pub struct AlephWeight<T>(PhantomData<T>);
 impl<T: frame_system::Config> BenchmarkInfo for AlephWeight<T> {
     /// The range of component `x` is `[0, 10000000]`.
     fn verify_read_args(x: u32, ) -> Weight {
-        // Minimum execution time: 554 nanoseconds.
-        Weight::from_parts(91_264_582_u64, 0)
+        // Minimum execution time: 254 nanoseconds.
+        Weight::from_parts(66_178_198_u64, 0)
             // Standard Error: 6
-            .saturating_add(Weight::from_parts(143_u64, 0).saturating_mul(x as u64))
+            .saturating_add(Weight::from_parts(150_u64, 0).saturating_mul(x as u64))
     }
+    // Storage: `VkStorage::VerificationKeys` (r:1 w:0)
+    // Proof: `VkStorage::VerificationKeys` (`max_values`: None, `max_size`: None, mode: `Measured`)
     fn verify_proof() -> Weight {
-        // Minimum execution time: 181 nanoseconds.
-        Weight::from_parts(203_000_u64, 0)
+        // Minimum execution time: 4_024_837 nanoseconds.
+        Weight::from_parts(4_128_288_000_u64, 0)
+            .saturating_add(T::DbWeight::get().reads(1_u64))
     }
 }
 
@@ -79,13 +82,16 @@ impl<T: frame_system::Config> BenchmarkInfo for AlephWeight<T> {
 impl BenchmarkInfo for () {
     /// The range of component `x` is `[0, 10000000]`.
     fn verify_read_args(x: u32, ) -> Weight {
-        // Minimum execution time: 554 nanoseconds.
-        Weight::from_parts(91_264_582_u64, 0)
+        // Minimum execution time: 254 nanoseconds.
+        Weight::from_parts(66_178_198_u64, 0)
             // Standard Error: 6
-            .saturating_add(Weight::from_parts(143_u64, 0).saturating_mul(x as u64))
+            .saturating_add(Weight::from_parts(150_u64, 0).saturating_mul(x as u64))
     }
+    // Storage: `VkStorage::VerificationKeys` (r:1 w:0)
+    // Proof: `VkStorage::VerificationKeys` (`max_values`: None, `max_size`: None, mode: `Measured`)
     fn verify_proof() -> Weight {
-        // Minimum execution time: 181 nanoseconds.
-        Weight::from_parts(203_000_u64, 0)
+        // Minimum execution time: 4_024_837 nanoseconds.
+        Weight::from_parts(4_128_288_000_u64, 0)
+            .saturating_add(RocksDbWeight::get().reads(1_u64))
     }
 }

bin/node/src/main.rs
@@ -1,16 +1,14 @@
 mod pruning_config;
 
-#[cfg(any(feature = "try-runtime", feature = "runtime-benchmarks"))]
-use aleph_node::ExecutorDispatch;
 use aleph_node::{new_authority, new_partial, Cli, Subcommand};
-#[cfg(any(feature = "try-runtime", feature = "runtime-benchmarks"))]
-use aleph_runtime::Block;
 use log::info;
 use primitives::HEAP_PAGES;
 use pruning_config::PruningConfigValidator;
 use sc_cli::{clap::Parser, SubstrateCli};
 use sc_network::config::Role;
 use sc_service::{Configuration, PartialComponents};
+#[cfg(any(feature = "try-runtime", feature = "runtime-benchmarks"))]
+use {aleph_node::ExecutorDispatch, aleph_runtime::Block, sc_executor::NativeExecutionDispatch};
 
 fn enforce_heap_pages(config: &mut Configuration) {
     config.default_heap_pages = Some(HEAP_PAGES);
@@ -120,7 +118,7 @@ fn main() -> sc_cli::Result<()> {
             let runner = cli.create_runner(cmd)?;
             runner.sync_run(|config| {
                 if let frame_benchmarking_cli::BenchmarkCmd::Pallet(cmd) = cmd {
-                    cmd.run::<Block, ()>(config)
+                    cmd.run::<Block, <ExecutorDispatch as NativeExecutionDispatch>::ExtendHostFunctions>(config)
                 } else {
                     Err(sc_cli::Error::Input("Wrong subcommand".to_string()))
                 }